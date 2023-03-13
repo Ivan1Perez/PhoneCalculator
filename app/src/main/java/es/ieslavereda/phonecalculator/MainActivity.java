ee5221023e7f2de54a320b0f3e62e122419c9d72
Ivan Perez <ivan_psh@hotmail.com>
2023-03-13 19:45:05 +0100

    Update

--- app/src/main/java/es/ieslavereda/phonecalculator/MainActivity.java
+++ app/src/main/java/es/ieslavereda/phonecalculator/MainActivity.java
@@ -2,13 +2,44 @@ package es.ieslavereda.phonecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button b1;
+    private TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+
+        // Oculta la barra de navegación y la barra de estado
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        // Detecta la interacción del usuario y vuelve a mostrar la barra de navegación y la barra de estado
+        decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+        });
+    }
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View view) {
+        if(display.getText().equals("0"))
+            display.setText(((Button)view).getText());
+        else
+            display.setText(String.valueOf(display.getText())+((Button)view).getText());
+
     }
 }
